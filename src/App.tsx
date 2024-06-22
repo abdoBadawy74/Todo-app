9ffecd30182a5b2e8ef9ac332f5069c3e6c895b7
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-06-22 14:12:49 +0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 // src/App.tsx
-import React from "react";
-import TodoApp from './Components/TodoApp';
-import ZustandTodoApp from './Components/ZustandTodoApp';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import TodoApp from "./Components/TodoApp.tsx";
+import ZustandTodoApp from './Components/ZustandTodoApp.tsx';
 
 const App: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <TodoApp />
-
-      <p style={{height:"5px",width:"100%",margin:"50px 0", background:"black"}}></p>
-      <ZustandTodoApp />
-    </div>
+    <Router>
+      <div className="container mx-auto p-4">
+        <nav className="mb-4">
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/redux" className="text-blue-500 hover:underline">Todo List (Redux)</Link>
+            </li>
+            <li>
+              <Link to="/zustand" className="text-blue-500 hover:underline">Todo List (Zustand)</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/redux" element={<TodoApp />} />
+          <Route path="/zustand" element={<ZustandTodoApp />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
